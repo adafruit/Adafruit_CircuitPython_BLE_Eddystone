6956f64fe41be36ec2f43e26db804c5bebfb03d3
dherrada <dylan.herrada@adafruit.com>
2021-02-03 16:27:23 -0500

    Ran pre-commit, added licenses

--- examples/ble_eddystone_simpletest.py
+++ examples/ble_eddystone_simpletest.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
 """This example broadcasts our Mac Address as our Eddystone ID and a link to the Adafruit Discord
    server."""
 
